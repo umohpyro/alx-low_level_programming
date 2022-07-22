fca099ff6aa39ebec037b2a999c208a45c2455f5
Umoh Andem <andemumoh@gmail.com>
2022-07-22 14:20:14 -0700

    0. It ain't what they call you, it's what you answer to - Write a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is repo is for 0x0A. C - argc, argv
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
+
+#endif /* MAIN_H */
