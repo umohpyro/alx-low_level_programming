5746daf517571bc70bb1997c983ce32824727a0d
Umoh Andem <andemumoh@gmail.com>
2022-07-25 12:56:16 -0700

    3. If you even dream of beating me you'd better wake up and apologize - Write a function that returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,9 +2,11 @@
 #define _MAIN_H_
 
 #include <stdlib.h>
+#include <stdio.h>
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 #endif
