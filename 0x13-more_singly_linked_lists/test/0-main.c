07c6d2e4fcb3c276b4d9a01dd3b15f998ee5ace9
Umoh Andem <andemumoh@gmail.com>
2022-08-15 14:52:34 -0700

    4. Free list - Write a function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/test/0-main.c
+++ 0x13-more_singly_linked_lists/test/0-main.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "lists.h"
+#include "../lists.h"
 
 /**
  * main - check the code
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
