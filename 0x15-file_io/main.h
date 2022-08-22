69e6e2160f246081d544bb9b2fd67da19b922a01
Umoh Andem <andemumoh@gmail.com>
2022-08-22 11:47:30 -0700

    1. Under the snow - Create a function that creates a file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef _MAIN_H
+#define _MAIN_H
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -8,5 +8,6 @@
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
